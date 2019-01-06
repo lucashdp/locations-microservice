2bd4e314459d785c6123a23140c1ed7d078a772d
Lucas Duarte <lucashdp@outlook.com>
2019-01-06 13:09:54 -0200

    Implements the simple web api for locations

---
+++ locations-microservice/Controllers/LocationsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace locations_microservice.Controllers
+{
+    [Route("v1/public/[controller]")]
+    [ApiController]
+    public class LocationsController : Controller
+    {
+        // GET v1/public/locations
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "location1", "location2" };
+        }
+
+        // GET v1/public/locations/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "location";
+        }
+
+        // POST v1/public/locations
+        [HttpPost]
+        public void Post([FromBody] string location)
+        {
+        }
+
+        // PUT v1/public/locations/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string location)
+        {
+        }
+
+        // DELETE v1/public/locations/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
